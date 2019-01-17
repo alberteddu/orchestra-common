e9989e5c5f4691b3d8cb8c34f56505b78cc1ff7b
Alberto Piu <alberto.piu@flixbus.com>
2019-01-17 10:58:19 +0100

    AAAAH

--- fragment.js
+++ fragment.js
@@ -6,13 +6,13 @@ const server = http.createServer((req, res) => {
     const pathname = url.parse(req.url).pathname;
     const jsHeader = { 'Content-Type': 'application/javascript' };
     switch (pathname) {
-        case '/public/bundle.js':
+        case '/common/public/bundle.js':
             res.writeHead(200, jsHeader);
             return fs.createReadStream('./public/bundle.js').pipe(res);
         default:
             res.writeHead(200, {
                 'Content-Type': 'text/html',
-                'Link': '<http://localhost:6006/public/bundle.js>; rel="fragment-script"',
+                'Link': '<http://orchestra.lbr.to/common/public/bundle.js>; rel="fragment-script"',
             });
             return res.end('')
     }
